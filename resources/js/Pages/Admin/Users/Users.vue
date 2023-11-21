5825ea877dcc173904dff36cd1d7b7ca6596ff6b
Igor Bilet <bilet.igor@gmail.com>
2023-11-21 14:29:15 +0200

    Изменен дизайн кнопок edit, delete в списке пользователей

--- resources/js/Pages/Admin/Users/Users.vue
+++ resources/js/Pages/Admin/Users/Users.vue
@@ -23,7 +23,7 @@
                     <td>{{ user.email }}</td>
                     <td>{{ user.formatted_created_at }}</td>
                     <td>
-                        <a type="button" class="btn btn-xs btn-outline-secondary rounded-1 me-2">Edit</a>
+                        <a type="button" class="btn btn-xs btn-outline-success rounded-1 me-2">Edit</a>
                         <a @click="destroy(user.id)" type="button" class="btn btn-xs btn-outline-danger rounded-1">Delete</a>
                     </td>
                 </tr>
