ef627e5db8f4485efa4f9a920b6ec8f2d8a6ec1b
Igor Bilet <bilet.igor@gmail.com>
2023-11-24 19:43:40 +0200

    Измненен footer в админке

--- resources/js/Components/Admin/Footer.vue
+++ resources/js/Components/Admin/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="bg-secondary rounded-3 mt-3 mb-2">
+    <footer class="bg-secondary rounded-top mt-3">
         <div class="container d-flex justify-content-between py-3">
             <div>
                 <span class="fw-bold link-light">{{ $page.props.appName }}</span> <span class="link-light">© {{ currentYear }}</span>
