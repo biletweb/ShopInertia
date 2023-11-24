4137a55d8a5d8ef592fe8c926a222d4f438158d0
Igor Bilet <bilet.igor@gmail.com>
2023-11-24 23:48:22 +0200

    Добален перевод для валидации форм в админке

--- resources/js/Pages/Admin/Users/Edit.vue
+++ resources/js/Pages/Admin/Users/Edit.vue
@@ -10,28 +10,28 @@
                     <label for="name" class="form-label">{{ $t('Name') }}</label>
                     <input v-model="form.name" type="text" name="name" class="form-control" :class="{ 'is-invalid': form.errors.name }" id="name" :placeholder="$t('Enter name')" />
                     <div v-if="form.errors.name" class="invalid-feedback">
-                        {{ form.errors.name }}
+                        {{ $t(form.errors.name) }}
                     </div>
                 </div>
                 <div class="mb-3">
                     <label for="email" class="form-label">{{ $t('Email') }}</label>
                     <input v-model="form.email" type="text" name="email" class="form-control" :class="{ 'is-invalid': form.errors.email }" id="email" :placeholder="$t('Enter email')" />
                     <div v-if="form.errors.email" class="invalid-feedback">
-                        {{ form.errors.email }}
+                        {{ $t(form.errors.email) }}
                     </div>
                 </div>
                 <div class="mb-3">
                     <label for="current_password" class="form-label">{{ $t('Current password') }}</label>
                     <input v-model="form.current_password" type="password" name="current_password" class="form-control" :class="{ 'is-invalid': form.errors.current_password }" id="current_password" :placeholder="$t('Enter password')" />
                     <div v-if="form.errors.current_password" class="invalid-feedback">
-                        {{ form.errors.current_password }}
+                        {{ $t(form.errors.current_password) }}
                     </div>
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label">{{ $t('New password') }}</label>
                     <input v-model="form.password" type="password" name="password" class="form-control" :class="{ 'is-invalid': form.errors.password }" id="password" :placeholder="$t('Enter password')" />
                     <div v-if="form.errors.password" class="invalid-feedback">
-                        {{ form.errors.password }}
+                        {{ $t(form.errors.password) }}
                     </div>
                 </div>
                 <div class="mb-3">
