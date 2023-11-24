cc02dcaf76756f4e1efd433a8e6a6d78fdb23b70
Igor Bilet <bilet.igor@gmail.com>
2023-11-24 22:48:08 +0200

    Почищен navbar в админке

--- resources/js/Components/Admin/Navbar.vue
+++ resources/js/Components/Admin/Navbar.vue
@@ -1,11 +1,10 @@
 <template>
-    <nav class="navbar navbar-expand-md navbar-dark bg-secondary rounded-3 mt-2 mb-3">
-        <div class="container-fluid">
+    <nav class="navbar navbar-expand-md bg-secondary rounded-bottom mb-3">
+        <div class="container">
             <Link class="navbar-brand" :href="route('admin.index')">{{ $t('ADMIN PANEL') }}</Link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-ex-7">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
             <div class="collapse navbar-collapse" id="navbar-ex-7">
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item dropdown">
@@ -15,16 +14,15 @@
                         </div>
                     </li>
                 </ul>
-                <ul class="navbar-nav ms-md-auto">
+                <ul class="navbar-nav">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="javascript:void(0)" data-bs-toggle="dropdown">{{ $t('Language') }}</a>
                         <div class="dropdown-menu">
                             <button @click="changeLanguage('uk')" class="dropdown-item"><img src="../../src/img/flags/ukraine.png" style="max-width: 25px;" class="me-2">{{ $t('Ukrainian') }}</button>
-                            <!-- <button class="dropdown-item"><img src="../../src/img/flags/russia.png" style="max-width: 25px;" class="me-2">{{ $t('Russian') }}</button> -->
-                            <button @click="changeLanguage('en')" class="dropdown-item" :href="route('users.index')"><img src="../../src/img/flags/english.png" style="max-width: 25px;" class="me-2">{{ $t('English') }}</button>
+                            <button @click="changeLanguage('en')" class="dropdown-item"><img src="../../src/img/flags/english.png" style="max-width: 25px;" class="me-2">{{ $t('English') }}</button>
                         </div>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item" style="margin-right: -7px;">
                         <Link class="nav-link" :href="route('shop.index')">{{ $t('Return to store') }}</Link>
                     </li>
                 </ul>
@@ -47,7 +45,3 @@ export default {
     }
 }
 </script>
-
-<style>
-    
-</style>
