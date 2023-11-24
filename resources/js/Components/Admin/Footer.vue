e8a5e280a58f6dbf25ec1d5a421c0c29b55b47c3
Igor Bilet <bilet.igor@gmail.com>
2023-11-24 19:28:32 +0200

    Почищен footer в админке

--- resources/js/Components/Admin/Footer.vue
+++ resources/js/Components/Admin/Footer.vue
@@ -1,18 +1,17 @@
 <template>
-    <footer class="content-footer footer bg-secondary rounded-3 mt-3 mb-2">
-        <div class="container-fluid d-flex flex-md-row flex-column justify-content-between align-items-md-center gap-1 py-3">
+    <footer class="bg-secondary rounded-3 mt-3 mb-2">
+        <div class="container d-flex justify-content-between py-3">
             <div>
-                <span class="footer-text fw-bold link-light">{{ $page.props.appName }}</span> <span class="link-light">© {{ currentYear }}</span>
+                <span class="fw-bold link-light">{{ $page.props.appName }}</span> <span class="link-light">© {{ currentYear }}</span>
             </div>
             <div>
-                <span class="footer-link fw-bold link-light">{{ $t('Version') }} {{ $page.props.appVersion }}</span>
+                <span class="fw-bold link-light">{{ $t('Version') }} {{ $page.props.appVersion }}</span>
             </div>
         </div>
     </footer>
 </template>
 
 <script>
-
 export default {
     data() {
         return {
@@ -21,7 +20,3 @@ export default {
     }
 }
 </script>
-
-<style>
-    
-</style>
